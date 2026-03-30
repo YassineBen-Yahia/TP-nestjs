c68a58a66ed93ef57d7f3470ada1108cc68a8869
yassine <yassinebnyahia@gmail.com>
2026-03-30 13:30:43 +0100

    update Cv, Skill, and User modules to include TypeOrmModule imports and exports

--- src/cv/cv.module.ts
+++ src/cv/cv.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CvService } from './cv.service';
 import { CvController } from './cv.controller';
+import { Cv } from './entities/cv.entity';
+import { User } from '../user/entities/user.entity';
+import { Skill } from '../skill/entities/skill.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Cv, User, Skill])],
   controllers: [CvController],
   providers: [CvService],
 })
--- src/skill/skill.module.ts
+++ src/skill/skill.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { SkillService } from './skill.service';
 import { SkillController } from './skill.controller';
+import { Skill } from './entities/skill.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Skill])],
   controllers: [SkillController],
   providers: [SkillService],
+  exports: [SkillService, TypeOrmModule],
 })
 export class SkillModule {}
