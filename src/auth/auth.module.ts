8c562fa86bdfad803d32da88694982e67ea9bffb
Chatti Mohamed Louay <mohamedlouay.chatti@insat.ucar.tn>
2026-04-02 22:52:18 +0100

    Updating auth module to JWT providers and exports

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,6 +6,8 @@ import { User } from 'src/user/entities/user.entity';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './jwt.strategy';
+import { JwtAuthGuard } from './jwt-auth.guard';
 
 @Module({
   imports: [
@@ -21,6 +23,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy, JwtAuthGuard],
+  exports: [JwtAuthGuard],  
 })
 export class AuthModule {}
